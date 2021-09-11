496edf469cc9d02df058ae7850d204e4b3985900
Rizki Firdaus <muhammadrizkifirdaus0710@gmail.com>
2021-09-11 11:50:55 +0700

    Learning Progres C++

---
+++ DigitCounter.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<" Please Enter Your Number : ";
+    cin >>number;
+
+    if(number==0)
+    {
+    cout<<" You Have Entered 0";
+    }
+    else 
+    {
+     int counter = 0;
+     while(number>0)
+     {
+        number = number/10;
+        counter++;
+     }
+     cout<<"You Have Entered "<<counter<<" Digits"<<endl;
+    }
+
+    return 0;
+}
