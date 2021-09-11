7e021b596687f9c7486b67c574d9ddc35d676e23
Rizki Firdaus <muhammadrizkifirdaus0710@gmail.com>
2021-09-11 12:43:10 +0700

    Learning Progress C++

--- Calculator.cpp
+++ Calculator.cpp
@@ -16,18 +16,20 @@ int main()
         case '/':cout<<num1<<operation<<num2<< "=" << num1/num2;break;
         case '%':
 
-        bool isnum1int,isnum2int;
+        bool isnum1int;
+        bool isnum2int;
 
-        isnum1int==(int)num1==num1;
-        isnum2int==(int)num2==num2;
+        isnum1int=((int)num1==num1);
+        isnum2int=((int)num2==num2);
 
         if(isnum1int && isnum2int)
-        {
-            cout<<num1<<operation<<num2<< "-"<<isnum1int%isnum2int;
-        }
-        else{
+        
+            cout<<num1<<operation<<num2<< "="<<(int)num1 % (int)num2;
+    
+        else
+        
             cout<<"NON VALID INPUT!";
-        }
+        
         break;
         default :
         cout<<"NON VALID OPERATION"<<endl;
